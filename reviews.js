a5ed70d98d5f133a5557d21b908daeac35fe9ae7
garazi <greg@assortedgarbage.com>
2015-05-13 14:59:13 -0700

    reviews API and controller

---
+++ reviews.js
@@ -0,0 +1,17 @@
+var request = require('request');
+
+module.exports = {
+    postReview: postReview
+};
+
+function postReview(req, res) {
+    request.post('http://api.usergrid.com/YOURORGNAME/sandbox/reviews', {
+        form: JSON.stringify(req.body)
+    }, function(error, response, body) {
+        if (error) {
+            res.send(error);
+        } else {
+            res.send(body);
+        }
+    });
+}
