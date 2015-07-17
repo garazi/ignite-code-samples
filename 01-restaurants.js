4b597a283b659247706386e7aa92d1451a4509b6
garazi <greg@assortedgarbage.com>
2015-07-17 13:07:33 -0700

    added /restaurants/{id}

--- 01-restaurants.js
+++ 01-restaurants.js
@@ -1,11 +1,25 @@
 var request = require("request");
  
 module.exports = {
-    getRestaurants: getRestaurants
+    getRestaurants: getRestaurants,
+    getRestaurantByID: getRestaurantByID
 };
  
 function getRestaurants(req, res) {
     request("http://api.usergrid.com/YOURORGNAME/sandbox/restaurants?limit=999", function(err, resp, body) {
+        if (err) {
+            res.send(err);
+        } else {
+            var results = {};
+            results.entities = JSON.parse(body).entities;
+            res.send(results);
+        }
+    });
+}
+
+function getRestaurantByID(req, res) {
+    var restID = req.swagger.params.id.value;
+    request("http://api.usergrid.com/YOURORGNAME/sandbox/restaurants?limit=999&ql=restID=" + restID, function(err, resp, body) {
         if (err) {
             res.send(err);
         } else {
